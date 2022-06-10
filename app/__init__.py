b31c77203a3825e0ebdd0c760d1f1a2d9c92e340
Fengwei Yu <fengwei.yu@lewagon.com>
2022-06-10 10:40:29 +0200

    upload the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,25 @@
 # app/__init.py__
 # pylint: disable=missing-docstring
 
+
 from flask import Flask
 from flask_restx import Api
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    from config import Config
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    from app.models import Tweet
+    from flask_migrate import Migrate
+
+    migrate = Migrate(app, db)
 
     from .apis.tweets import api as tweets
     api = Api()
---
+++ app/models.py
@@ -0,0 +1,15 @@
+# app/models.py
+# pylint: disable=missing-docstring
+
+from datetime import datetime
+
+from app import db
+
+class Tweet:
+    __tablename__ = "tweets"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(280))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Tweet #{self.id}>"
